2f89c59ec485cc7b4422e1d4dac66e8a51b2c8fc
ThePersonThat <gaudabrus@inbox.ru>
2021-09-24 14:19:14 +0300

    Added music listening page

---
+++ src/main/resources/static/js/music.js
@@ -0,0 +1,32 @@
+export const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+    return `${min}:${sec}`;
+}
+
+function Sound(track) {
+    this.sound = track;
+    this.getDuration = () => formatTime(this.sound.duration());
+    this.getCurrentTime = () => formatTime(this.sound.seek());
+    this.setCurrentTime = (seconds) => this.sound.seek(seconds);
+    this.getNotFormatedTime = () => this.sound.seek();
+    this.getNotFormatedDuration = () => this.sound.duration();
+    this.play = () => this.sound.play();
+    this.pause = () => this.sound.pause();
+    this.once = (event, fn) => this.sound.once(event, fn);
+}
+
+export function load(url) {
+    const track = new Howl({
+        src: [url],
+        format: ["mp3"],
+    });
+
+    return new Sound(track);
+}
